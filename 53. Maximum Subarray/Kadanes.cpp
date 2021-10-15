1a574f1c1fd8d080c81eeb4b227269abcdc3afad
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-10-15 20:49:46 +0800

    Update Kadanes.cpp

--- 53. Maximum Subarray/Kadanes.cpp
+++ 53. Maximum Subarray/Kadanes.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_current = nums[0];
-        int max_global = nums[0];
-        
-        for(int i = 1; i < nums.size(); i++){
-            max_current = max(max_current+nums[i], nums[i]);
-            if(max_current > max_global){
-                max_global = max_current;
-            }
+        int ans = INT_MIN, current_sum = 0;
+        for(auto i : nums){
+            current_sum += i;
+            ans = max(current_sum, ans);
+            current_sum = max(current_sum, 0);
         }
-        return max_global;
+        return ans;
     }
 };
