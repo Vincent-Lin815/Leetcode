3e4f2223669effc849fb0cb1fdeb0a0767a28e01
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-11-18 19:51:35 +0800

    Create array.cpp

---
+++ HashSet & HashMap/Easy/387. First Unique Character in a String/array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int list[256] = {0};
+        for(auto i: s)
+            list[i] ++;
+        for(int i=0; i<s.length();i++)
+            if(list[s[i]]==1) return i;
+        return -1;
+    }
+};
