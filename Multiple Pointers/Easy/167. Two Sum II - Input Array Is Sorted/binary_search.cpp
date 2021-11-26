085419a718f4b0508c9cf7dbc351e6c8cdc602e2
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-11-26 21:47:48 +0800

    Create binary_search.cpp

---
+++ Multiple Pointers/Easy/167. Two Sum II - Input Array Is Sorted/binary_search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int N = nums.size();
+        for(int i=0;i<N-1;i++){
+            int lo = i+1;
+            int hi = N;
+            while(lo<hi){
+                int mid = lo + (hi-lo)/2;
+                if(nums[mid]==target-nums[i]) return {i+1,mid+1};
+                else if(nums[mid]>target-nums[i]) hi=mid;
+                else lo = mid+1;
+            }
+            if(lo!=nums.size() && nums[lo]==target-nums[i]) return {i+1,lo+1};
+        }
+        return {}; // not found
+    }
+};
