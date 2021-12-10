a89fb19c4cba6a3009efd3abf7dbd114385bfeab
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-12-10 12:38:47 +0800

    Create solution.cpp

---
+++ Binary Search/Easy/374. Guess Number Higher or Lower/solution.cpp
@@ -0,0 +1,29 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int ans, left = 1, right = n;
+        
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            
+            if(guess(mid) == 1){
+                left = mid + 1;
+            }else if(guess(mid) == -1){
+                right = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        
+        return 0;
+    }
+};
