a5a201d679dfe8321dea0ba21afeb843793aaa82
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-12-11 22:34:38 +0800

    Create BFS.cpp

---
+++ Tree/Easy/104. Maximum Depth of Binary Tree/BFS.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        
+        int depth = 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            depth++;
+            int n = q.size();
+            
+            for(int i = 0; i < n; i++){
+                TreeNode *node = q.front();
+                q.pop();
+                
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+            }
+        }
+        
+        return depth;
+    }
+};
