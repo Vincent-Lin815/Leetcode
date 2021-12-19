a7c50515bfc0a27160e4e3d98ac70ca7ddb025e9
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-12-19 22:57:09 +0800

    Update solution.cpp

--- Array & String/Medium/5. Longest Palindromic Substring/solution.cpp
+++ Array & String/Medium/5. Longest Palindromic Substring/solution.cpp
@@ -1,1 +1,27 @@
-TODO
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.size() < 2) return s;
+        
+        int maxlen = 1, max_left = 0;
+        
+        for(int mid = 0; mid < s.size(); mid++){
+            // odd len
+            int left = mid, right = mid;
+            
+            while(right < s.size()-1 && s[right] == s[right+1]) right++;
+            
+            while(left > 0 && right < s.size()-1 && s[left-1] == s[right+1]){
+                left--;
+                right++;
+            }
+            
+            if(right-left+1 > maxlen){
+                maxlen = right-left+1;
+                max_left = left;
+            }
+        }
+        
+        return s.substr(max_left, maxlen);
+    }
+};
