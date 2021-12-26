2c76cc84a0c7dbde27fa8719899f7e530d206812
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-12-26 19:54:43 +0800

    Create bucket_sort.cpp

---
+++ Array & String/Medium/347. Top K Frequent Elements/bucket_sort.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        if (!nums.size()) return res;
+        unordered_map<int, int> cnt;
+        for (auto num : nums) cnt[num]++;
+        vector<vector<int>> bucket(nums.size() + 1);
+        for (auto kv : cnt) {
+            bucket[kv.second].push_back(kv.first);
+        }
+
+        for (int i = bucket.size() - 1; i >= 0; --i) {
+            for (int j = 0; j < bucket[i].size(); ++j){
+                res.push_back(bucket[i][j]);
+                if (res.size() == k) return res;
+            }
+        }
+
+        return res;
+    }
+};
