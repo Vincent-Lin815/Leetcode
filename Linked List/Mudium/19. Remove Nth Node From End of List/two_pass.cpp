50effe9281c5a03dbf7de404c33574eeb2702f75
Vincent-Lin <54383498+Vincent-Lin815@users.noreply.github.com>
2021-12-27 21:06:39 +0800

    Create two_pass.cpp

---
+++ Linked List/Mudium/19. Remove Nth Node From End of List/two_pass.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// two pass
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int length = 1;
+        ListNode* curr = head;
+        
+        while(curr->next != NULL){
+            curr = curr->next;
+            length++;
+        }
+        
+        curr = head;
+        
+        if(length-n == 0) return head->next;
+        
+        for(int i = 0; i < length-n-1; i++){
+            curr = curr->next;
+        }
+        
+        curr->next = curr->next->next;
+        
+        return head;
+    }
+};
